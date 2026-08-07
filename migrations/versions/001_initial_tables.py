55c5bbdf2a0b83d1f46a9ea2a0d3aa0573794d96
Maryam Wasay <maryamwasay3@gmail.com>
2026-08-06 23:19:25 -0700

    Create 001_initial_tables.py

---
+++ migrations/versions/001_initial_tables.py
@@ -0,0 +1,23 @@
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "001_initial_tables"
+
+down_revision = None
+
+branch_labels = None
+
+depends_on = None
+
+
+
+def upgrade():
+
+    pass
+
+
+
+def downgrade():
+
+    pass
