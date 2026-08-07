0643014c3220291dc104d5411932fea5eafa2c5e
Maryam Wasay <maryamwasay3@gmail.com>
2026-08-06 23:04:44 -0700

    Add files via upload

---
+++ app/auth/jwt.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from datetime import timedelta
+from typing import Optional
+
+from jose import jwt
+from jose import JWTError
+
+from app.config import settings
+
+
+def create_access_token(
+    data: dict,
+    expires_delta: Optional[timedelta] = None
+) -> str:
+
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + (
+        expires_delta
+        or timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
+
+
+def verify_token(token: str) -> Optional[dict]:
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            settings.JWT_SECRET_KEY,
+            algorithms=[settings.JWT_ALGORITHM]
+        )
+
+        return payload
+
+    except JWTError:
+
+        return None
