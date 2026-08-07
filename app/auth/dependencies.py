dd010d0f9251c0a0e908c36bedb5d3bf55259bc0
Maryam Wasay <maryamwasay3@gmail.com>
2026-08-06 23:04:24 -0700

    Create dependencies.py

---
+++ app/auth/dependencies.py
@@ -0,0 +1,24 @@
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+from app.auth.jwt import verify_token
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/login"
+)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme)
+):
+
+    payload = verify_token(token)
+
+    if payload is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+
+    return payload
